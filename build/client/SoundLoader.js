8570ef5cfab06c754b0756452a15a1e95300a4b0
Kevin <kevin.of.dahl@gmail.com>
2016-06-28 23:42:08 -0700

    removed more undesirables

--- build/client/SoundLoader.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var SoundLoader = (function () {
-    function SoundLoader() {
-    }
-    return SoundLoader;
-}());
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = SoundLoader;
-//# sourceMappingURL=SoundLoader.js.map
