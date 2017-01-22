301a0df5fcedae5f498c8e5652acbbe20e03e8fd
Kevin <kevin.of.dahl@gmail.com>
2017-01-21 16:12:42 -0800

    added timing to build script

--- build.py
+++ build.py
@@ -1,12 +1,26 @@
 import os
 import platform
 import re
+import time
+import subprocess
 
-browserify = 'browserify build/client/main.js -o public/js/mmoo-client.js'
+shellCommands = [
+	'tsc',
+	'browserify build/client/main.js -o public/js/mmoo-client.js'
+]
 
-if platform.system() == 'Windows':
-	browserify = re.sub(r'/', r'\\', browserify)
+startTime = time.clock()
+for command in shellCommands:
+	if platform.system() == 'Windows':
+		command = re.sub(r'/', r'\\', command)
 
-#print(command)
-os.system('tsc')
-os.system(browserify)
+	commandStartTime = time.clock()
+	print('Running "{0}"...'.format(command))
+
+	subprocess.call(command, shell=True)
+
+	commandTime = time.clock() - commandStartTime
+	print('Finished in {0:.2f} seconds\n'.format(round(commandTime,2)))
+
+totalTime = time.clock()
+print('Build completed in {0:.2f} seconds'.format(round(totalTime,2)))
